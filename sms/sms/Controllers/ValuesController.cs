6147cc0523cd9a1e992085b6a2d453c003502a16
tasherul <47455913+tasherul@users.noreply.github.com>
2019-03-12 16:44:03 +0600

    12/3/2019

--- sms/sms/Controllers/ValuesController.cs
+++ sms/sms/Controllers/ValuesController.cs
@@ -10,30 +10,37 @@ namespace sms.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
+        static List<string> strings = new List<string>(){
+        "value0","value1", "value2"
+        };
+
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return strings;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return strings[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            strings.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            strings[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            strings.RemoveAt(id);
         }
     }
 }
